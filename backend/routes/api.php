85f7f3677f9f14525df1848f5a98c96d5c335764
Abner <agalvez@hotelrevenue.com>
2025-12-11 16:12:42 -0300

    feat[BACK/routes] Se agregan rutas principales CRUD y proceso de juego

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,35 +20,67 @@ $router->group(['prefix' => 'api'], function () use ($router) {
             $router->get('users', 'UserController@index');
 
             // Rutas CRUD para administración de trivias
+            $router->get('trivias', 'TriviaController@index');
+            $router->post('trivias', 'TriviaController@store');
+            $router->get('trivias/{id}', 'TriviaController@show');
+            $router->put('trivias/{id}', 'TriviaController@update');
+            $router->delete('trivias/{id}', 'TriviaController@destroy'); // borrado de cascada y validacion de existencia en respuestas.
+            $router->get('trivias_users/{id}', 'TriviaController@triviaUsers');
 
             // Rutas CRUD para administración de preguntas y opciones
+            $router->get('trivias/{id}/questions', 'QuestionController@index');
+            $router->post('trivias/{id}/questions', 'QuestionController@store');
+            $router->get('questions/{id}', 'QuestionController@show');
+            $router->put('questions/{id}', 'QuestionController@update');
+            $router->delete('questions/{id}', 'QuestionController@destroy'); // borrado de cascada y validacion de existencia en respuestas.
 
             // Rutas CRUD para administración de niveles
+            $router->get('levels', 'LevelController@index');
+            $router->post('levels', 'LevelController@store');
+            $router->get('levels/{id}', 'LevelController@show');
+            $router->put('levels/{id}', 'LevelController@update');
+            $router->delete('levels/{id}', 'LevelController@destroy'); // borrado de cascada y validacion de existencia en preguntas.
 
             // Rutas CRUD para administración de roles
+            $router->get('roles', 'RoleController@index');
+            $router->post('roles', 'RoleController@store');
+            $router->get('roles/{id}', 'RoleController@show');
+            $router->put('roles/{id}', 'RoleController@update');
+            $router->delete('roles/{id}', 'RoleController@destroy'); // validacion de existencia en usuarios.
 
             // Rutas para visualizacion de respuestas y resultados de usuarios
+            $router->get('trivias/{id}/responses', 'TriviaController@responses');
+            $router->get('trivias/{id}/responses/{user_id}', 'TriviaController@userResponses');
 
             // Rutas para asignar trivias a usuarios
-
-            // Ruta para asignar roles a usuarios
+            $router->post('trivias/{id}/assign', 'TriviaController@assignToUsers'); // Asignacion por email
+            
+            // Ruta para asignar rol admin a usuario
+            $router->post('users/{id}/role_assign', 'UserController@assignRole');
         });
 
-        // Rutas  para "player" (o cualquier usuario autenticado)
+        //##### Rutas  para "player" (o cualquier usuario autenticado) ################
 
         // Ver mis trivias asignadas y puntajes obtenidos
+        $router->get('assigned_trivias', 'UserController@assignedTrivias'); // puntaje = null a las no completadas. 
 
         // Entrar a una trivia y ver preguntas + opciones
+        $router->get('trivias/{id}/get_full', 'TriviaController@getFullTrivia'); // trivia + preguntas + opciones
         
         // Responder todas las preguntas de una trivia
+        $router->post('trivias/{id}/answer_all', 'TriviaController@answerAllQuestions');
 
         // Responder solo una pregunta
+        $router->post('questions/{id}/answer', 'QuestionController@answerQuestion');
 
         // Ver mis respuestas y resultado final de una trivia
+        $router->get('trivias/{id}/my_answers', 'TriviaController@myAnswer');
 
-        // 
-        $router->get('trivias', 'TriviaController@index');
+        // Ver el ranking general de una trivia (todos los usuarios)
+        $router->get('trivias/{id}/ranking', 'TriviaController@ranking');
 
+        // Ver el ranking general de todas las trivias (todos los usuarios)
+        $router->get('all_trivias_ranking', 'TriviaController@allTriviasRanking');
 
     });
 });
